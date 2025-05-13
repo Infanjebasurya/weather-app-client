42580a0e91d4f0a0b51ddcb076237838878bd101
Infant Jeba Surya <infantjebasurya@gmail.com>
2025-05-13 08:02:12 +0000

    update API_BASE_URL

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api/weather';
+const API_BASE_URL = 'https://weather-app-server-two.vercel.app/api/weather';
 
 export const getWeatherByCity = async (city) => {
   try {
     const response = await axios.get(`${API_BASE_URL}/${city}`);
+    console.log(response)
     return response.data;
   } catch (error) {
     throw new Error(
